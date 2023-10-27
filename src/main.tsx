f5ea3dd62601b80cc890d36358b907c507ae0904
Ana Catarina Hristoff <ana.hristoff@grupoboticario.com>
2023-10-27 14:41:27 +0100

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const Strict = () => (
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+
+ReactDOM.render(<Strict />, document.getElementById('root'))
