2558bfb2cb367f21476eb6e471b5e97389de268e
Ana Catarina Hristoff <ana.hristoff@grupoboticario.com>
2023-10-27 15:07:47 +0100

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build:{
-    outDir: "build",
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    port: 8080
   }
 })
