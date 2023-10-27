1c47725c127c44fdb2d6e4b0d8779565207b3255
Ana Catarina Hristoff <ana.hristoff@grupoboticario.com>
2023-10-27 18:27:13 +0100

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: "casamento-frontend",
   server: {
     port: 8080
   }
